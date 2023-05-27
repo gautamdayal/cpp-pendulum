ab4af5f70de31d6572d72587fe7a50a9f38b47d8
Gautam Dayal <gautam.dayal@yahoo.com>
2023-05-27 12:36:41 -0700

    Solvers work

--- main.cpp
+++ main.cpp
@@ -11,8 +11,7 @@ double f(double t, double y) {
 
 
 int main() {
-    std::cout << "hello world" << std::endl;
-    ODESolver<FORWARD_EULER, double (*)(double, double)> quadratic_solver = ODESolver<FORWARD_EULER, double (*)(double, double)>((double (*)(double, double))f, 0.1, 0, 0);
+    ODESolver<RUNGE_KUTTA_4, double (*)(double, double)> quadratic_solver = ODESolver<RUNGE_KUTTA_4, double (*)(double, double)>((double (*)(double, double))f, 0.1, 0, 0);
     for (int i = 0; i < 100; i++) {
         std::cout << quadratic_solver.get_t() << " " << quadratic_solver.get_y() << std::endl;
         quadratic_solver.step();
