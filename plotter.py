8dad68f3c43321e8fdcbbbdbafff844c68c02cfc
Gautam Dayal <gautam.dayal@yahoo.com>
2023-05-27 12:38:27 -0700

    Plotting scripts

---
+++ plotter.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+euler = np.loadtxt("euler_output.txt")
+rk4 = np.loadtxt("rk4_output.txt")
+
+plt.plot(euler[:,1], label="Forward Euler")
+plt.plot(rk4[:,1], label="RK4")
+plt.plot((euler[:,0]**3)/3, label="Truth")
+plt.legend()
+plt.show()
