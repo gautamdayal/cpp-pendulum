63979801c636d4117463d6410df2eb1dfb74ec44
Gautam Dayal <gautam.dayal@yahoo.com>
2023-06-13 23:53:28 -0700

    Plotter addition

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -7,9 +7,19 @@ plt.rcParams["font.family"] = "monospace"
 data = np.loadtxt("logs/pendulum_out.txt")
 constants = data[0]
 g, L, c = constants[0], constants[1], constants[2]
-plt.plot(data[:,1][1::], data[:,2][1::])
+theta = data[:,1][1::]
+omega = data[:,2][1::]
+plt.plot(theta, omega)
 plt.xlabel("Angle")
 plt.ylabel("Angular Velocity")
 plt.title("Pendulum Phase Portrait")
 
 plt.savefig(f"plots/phase_portrait_g{g}_L{L}_c{c}.png")
+plt.clf()
+
+plt.plot(theta, label="Angle")
+plt.plot(omega, label="Angular Velocity")
+plt.xlabel("Time (s)")
+plt.ylabel("Angles (rad)")
+plt.legend()
+plt.savefig(f"plots/timeseries_g{g}_L{L}_c{c}.png")
